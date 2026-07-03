059240cf3427b97e7b352e8563aca89d7d651cb4
shouryaahhh <shouryaag659@gmail.com>
2026-07-03 21:53:36 +0530

    Hide admin behind secret URL

--- src/components/admin/ProtectedAdminRoute.tsx
+++ src/components/admin/ProtectedAdminRoute.tsx
@@ -8,7 +8,12 @@ export default function ProtectedAdminRoute({ children }: Props) {
   const token = localStorage.getItem("adminToken");
 
   if (!token) {
-    return <Navigate to="/admin/login" replace />;
+    return (
+  <Navigate
+    to="/mislona-detergent-admin-94100/login"
+    replace
+  />
+);
   }
 
   return <>{children}</>;
